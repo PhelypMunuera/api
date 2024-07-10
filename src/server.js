9319f9c39f91e1a2b39a102366dcb88c20db3528
Phelyp Munuera <mphelyp@gmail.com>
2024-07-10 20:56:27 -0300

    asyncerror

--- src/server.js
+++ src/server.js
@@ -1,20 +1,43 @@
 const express = require("express");
 const app = express();
 
+
 app.get("/message/:id/:user", (request, response)=> {
-const {id, user} = request.params;/*<-- atenção ao PARAMS  (OBRIGATÓRIO)*/
-
-    response.send(`O ID nº${id} é para o usuário ${user}`)
-})
-
-
-app.get("/users", (request, response)=> {
-    const { page, limit} = request.query; /*<-- atenção ao QUERY */
+    const {id, user} = request.params;/*<-- atenção ao PARAMS  (OBRIGATÓRIO)*/
     
-        response.send(`Pagina ${page} no Limite ${limit}`)
+        response.send(`O ID nº${id} é para o usuário ${user}`)
     })
-
-
+    
+    
+    app.get("/users", (request, response)=> {
+        const { page, limit} = request.query; /*<-- atenção ao QUERY */
+        
+            response.send(`Numero da pagina  ${page} no Limite ${limit}`)
+        })
+    
 const port = 3333;
 
 app.listen(port, () => console.log(`server is running on Port ${port}`))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
